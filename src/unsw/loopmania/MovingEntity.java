52001944d1a49c7abac05c87fa0c4b6a74b64e47
Tvisha Kubal <z5256001@ad.unsw.edu.au>
2021-07-04 21:04:21 +1000

    Removed Health attribute from constructor

--- src/unsw/loopmania/MovingEntity.java
+++ src/unsw/loopmania/MovingEntity.java
@@ -11,16 +11,14 @@ public abstract class MovingEntity extends Entity {
      * object holding position in the path
      */
     private PathPosition position;
-    private Health health;
 
     /**
      * Create a moving entity which moves up and down the path in position
      * @param position represents the current position in the path
      */
-    public MovingEntity(PathPosition position, Health health) {
+    public MovingEntity(PathPosition position) {
         super();
         this.position = position;
-        this.health = health;
     }
 
     /**
