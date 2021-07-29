06b5be547e3d6a06b0eb8ff193d36c24ead7abd9
Tvisha Kubal <z5256001@ad.unsw.edu.au>
2021-07-29 22:50:06 +1000

    Implemented Anduril and TreeStump rare items in a new folder

---
+++ src/unsw/loopmania/RareItems/Anduril.java
@@ -0,0 +1,42 @@
+package unsw.loopmania.RareItems;
+
+import unsw.loopmania.*;
+import unsw.loopmania.Character;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.File;
+
+/**
+ * Represents an Anduril, Flame of the West in the backend world
+ * An Anduril triples damage done to bosses
+ */
+public class Anduril extends StaticEntity{
+    
+    private int damage;
+    public Anduril(SimpleIntegerProperty x, SimpleIntegerProperty y) {
+        super(x, y, new ImageView(new Image((new File("src/images/anduril_flame_of_the_west.png")).toURI().toString())));
+    } 
+
+    public Anduril getAnduril() {
+        return this;
+    }
+    
+    /**
+     * Reduces doggie health by 3 times the normal damage
+     */
+    public void reduceDoggieHealth(Doggie doggie, Character character) {
+        
+        damage = character.getBaseDamage() * 3;
+        doggie.decreaseHealth(damage);
+    }
+
+    /**
+     * Reduces Elan Muske health by 3 times the normal damage
+     */
+    public void reduceElanMuskeHealth(ElanMuske elanMuske, Character character) {
+        
+        damage = character.getBaseDamage() * 3;
+        elanMuske.decreaseHealth(damage);
+    }
+}
---
+++ src/unsw/loopmania/RareItems/TreeStump.java
@@ -0,0 +1,37 @@
+package unsw.loopmania.RareItems;
+
+import unsw.loopmania.*;
+import unsw.loopmania.Character;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.File;
+
+/**
+ * Represents an Anduril, Flame of the West in the backend world
+ * An Anduril triples damage done to bosses
+ */
+public class TreeStump extends StaticEntity{
+    
+    public TreeStump(SimpleIntegerProperty x, SimpleIntegerProperty y) {
+        super(x, y, new ImageView(new Image((new File("src/images/anduril_flame_of_the_west.png")).toURI().toString())));
+    } 
+
+    public TreeStump getTreeStump() {
+        return this;
+    }
+    
+    /**
+     * Reduces doggie damage by 5 times
+     */
+    public void reduceDoggieDamage(Character character, Doggie doggie) {
+        character.decreaseHealth(doggie.getBaseDamage / 5);
+    }
+
+    /**
+     * Reduces Elan Muske damage by 5 times
+     */
+    public void reduceElanMuskeDamage(Character character, ElanMuske elanMuske) {
+        character.decreaseHealth(elanMuske.getBasedamage / 5);
+    }
+}
