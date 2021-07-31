81edf5e41db85351b10afa4c97902aa1577506dc
Tvisha Kubal <z5256001@ad.unsw.edu.au>
2021-07-31 23:49:03 +1000

    moved oneRing to RareItems

---
+++ src/unsw/loopmania/RareItems/TheOneRing.java
@@ -0,0 +1,24 @@
+package unsw.loopmania.RareItems;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import unsw.loopmania.StaticEntity;
+import unsw.loopmania.CharacterFolder.Character;
+
+import java.io.File;
+
+/**
+ * Basic Items
+ */
+public class TheOneRing extends StaticEntity {
+    
+    public TheOneRing(SimpleIntegerProperty x, SimpleIntegerProperty y) {
+        super(x,y,new ImageView(new Image((new File("src/images/the_one_ring.png")).toURI().toString())) );
+    }
+
+    public void reviveCharacter(Character c) {
+        c.increaseHealth(100);
+    }
+   
+}
